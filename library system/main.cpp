9cf2ec60241ea4fd251f5b18918e56bec2b6f610
VeN0mYT <mahmoud_spiderman@yahoo.com>
2025-04-01 16:54:24 +0200

    Update main.cpp

--- library system/main.cpp
+++ library system/main.cpp
@@ -12,12 +12,13 @@ int main()
 	/*
 	BookManager* bookmanager = BookManager::getInstance();  // getting the instance to the pointer so you can access the object from the pointer
 
-	bookmanager->addBook({ 5678, 2002, "C++" ,"abdo",false});
-	//bookmanager->addBook({ 5678, 2002, "C++" ,"abdo",false});
-	bookmanager->addBook({ 5646, 2002, "C++v2" ,"hesham",false});
+	bookmanager->AddBook({ 5678, 2002, "C++" ,"abdo",false});
+	//bookmanager->AddBook({ 5678, 2002, "C++" ,"abdo",false});
+	bookmanager->AddBook({ 5646, 2002, "C++v2" ,"hesham",false});
 	bookmanager->DisplayBook(5678);
-	bookmanager->updateBook({ 5678, 2045, "C++" ,"lolbo",false});
-	bookmanager->DisplayBook(5678);
+	BookManager* bookmanager2 = BookManager::getInstance();		// that pointer will point to the same object same data no new copy will be created
+	bookmanager2->updateBook({ 5678, 2045, "C++" ,"lolbo",false});
+	bookmanager2->DisplayBook(5678);
 	*/
 	/*
 	ClientManager* clientmanager = ClientManager::getInstance();
